7d14191455fe5b8dbec725bdaf85ddd2414b11b8
LincolnLink <link7-7@hotmail.com>
2025-04-16 13:12:09 -0300

    Testando o construtor de uma entidade, basico do teste unitario.

---
+++ FundamentosTestes/FundamentosTestes.Tests/Entities/CarTests.cs
@@ -0,0 +1,93 @@
+﻿
+using Bogus;
+using FluentAssertions;
+using FundamentosTestes.Entities;
+using Xunit.Abstractions;
+
+namespace FundamentosTestes.Tests.Entities
+{
+    [Trait("Category", "Car")]
+    public sealed class CarTests
+    {
+        private readonly Faker _faker = new("pt_BR");
+        private readonly ITestOutputHelper _testOutputHelper;
+
+        public CarTests(ITestOutputHelper testOutputHelper)
+        {
+            _testOutputHelper = testOutputHelper;
+        }
+
+        [Theory] //- Theory posibilita passar mais que um parametro.
+        [InlineData("Ferrari")]
+        [InlineData("Fusca")]
+        public void TheoryConstructor_GivenAllParameters_ThenShouldSetThePropertiesCorrectly(string expectedCarName)
+        {
+            // Given All Parameters
+            // Arrange - Arruma valores pro teste            
+            var expectedId = Guid.NewGuid();
+
+            // Act - Executa o teste
+            var carro = new Car(expectedId, expectedCarName);
+
+            // Then should set the properties correctly
+            // Assert - Definindo a regra do Teste.
+            Assert.Equal(expectedId, carro.Id);
+            Assert.Equal(expectedCarName, carro.Name);
+        }
+
+        [Fact] //- fato tem apenas uma condição.
+        public void NoFakerConstructor_GivenAllParameters_ThenShouldSetThePropertiesCorrectly()
+        {
+            // Given All Parameters
+            // Arrange - Arruma valores pro teste
+            var expectedCarName = "Carro";
+            var expectedId = Guid.NewGuid();
+
+            // Act - Executa o teste
+            var carro = new Car(expectedId, expectedCarName);
+
+            // Then should set the properties correctly
+            // Assert - Definindo a regra do Teste.
+            Assert.Equal(expectedId, carro.Id);
+            Assert.Equal(expectedCarName, carro.Name);
+        }
+
+        [Fact]
+        public void FakerConstructor_GivenAllParameters_ThenShouldSetThePropertiesCorrectly()
+        {
+            // Given All Parameters
+            // Arrange - Arruma valores pro teste            
+            var expectedCarName = _faker.Person.FirstName;
+            var expectedId = Guid.NewGuid();
+            _testOutputHelper.WriteLine(expectedCarName);
+
+            // Act - Executa o teste
+            var carro = new Car(expectedId, expectedCarName);
+
+            // Then should set the properties correctly
+            // Assert - Definindo a regra do Teste.
+            Assert.Equal(expectedId, carro.Id);
+            Assert.Equal(expectedCarName, carro.Name);
+        }
+
+        [Fact]
+        public void FluentAssertionsConstructor_GivenAllParameters_ThenShouldSetThePropertiesCorrectly()
+        {
+            // Given All Parameters
+            // Arrange
+            var expectedCarName = _faker.Person.FirstName;
+            var expectedId = Guid.NewGuid();
+            _testOutputHelper.WriteLine(expectedCarName);
+
+            // Act
+            var carro = new Car(expectedId, expectedCarName);
+
+            // Then should set the properties correctly
+            // Assert
+            // Assert.Equal(expectedId, carro.Id);
+            // Assert.Equal(expectedCarName, carro.Name);
+            carro.Id.Should().Be(expectedId, "Should be equal");
+            carro.Name.Should().Be(expectedCarName);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # DotNetTestesUnitarios
+
+ - Você deve deixar as interface publica e as classes de implementação internal.
+
+ - Referencia o projeto no seu projeto de teste, para ele poder saber qual projeto testar.
+   
+ - Adiciona uma configuração no csproj que faz o projeto de teste reconhecer as classes internal!
+
+ ```csharp
+
+    <ItemGroup>
+        <AssemblyAttribute Include="System.Runtime.CompilerServices.InternalsVisibleTo">
+            <_Parameter1>$(AssemblyName).Tests</_Parameter1>
+        </AssemblyAttribute>
+    </ItemGroup>
+
+ ```
+
+ - Bogus, biblioteca que gera valores automatico para ser testado.
+
+ - FluentAssertions, uma outra biblioteca para deixar a leitura mais bonita e tem varios metodos que ajuda atestar.
+
+ ```csharp
+
+            // Assert.Equal(expectedId, carro.Id);
+            // Assert.Equal(expectedCarName, carro.Name);
+            carro.Id.Should().Be(expectedId, "Should be equal");
+            carro.Name.Should().Be(expectedCarName);
+
+ ```
+
+  - Trait , Uma biblioteca que serve para descrever uma categoria de teste
